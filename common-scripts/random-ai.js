6dc2cb4bf87e0a70b6efd0f145228b032ef85d49
GlukKazan <glukkazan@gmail.com>
2017-07-07 20:20:51 +0300

    Random AI (refactoring)

--- common-scripts/random-ai.js
+++ common-scripts/random-ai.js
@@ -2,6 +2,9 @@
 
 function RandomAi(params) {
   this.params = params;
+  if (_.isUndefined(this.params.rand)) {
+      this.params.rand = _.random;
+  }
 }
 
 var findBot = Dagaz.AI.findBot;
@@ -15,35 +18,21 @@ Dagaz.AI.findBot = function(type, params, parent) {
 }
 
 RandomAi.prototype.setContext = function(ctx, board) {
-  if (!_.isUndefined(ctx.childs)) {
-      delete ctx.childs;
-  }
   ctx.board  = board;
 }
 
 RandomAi.prototype.getMove = function(ctx) {
-  if (_.isUndefined(ctx.childs)) {
-      ctx.childs = _.chain(Dagaz.AI.generate(ctx, ctx.board))
-       .map(function(move) {
-           return {
-              move: move
-           };
-        }, this)
-       .value();
-  }
-  if (ctx.childs.length == 0) {      
+  var moves = Dagaz.AI.generate(ctx, ctx.board);
+  if (moves.length == 0) {      
       return { done: true, ai: "nothing" };
   }
-  if (ctx.childs.length == 1) {
-      return { done: true, move: ctx.childs[0].move, ai: "once" };
+  if (moves.length == 1) {
+      return { done: true, move: moves[0], ai: "once" };
   }
-  if (_.isUndefined(this.params.rand)) {
-      this.params.rand = _.random;
-  }
-  var ix = this.params.rand(0, ctx.childs.length - 1);
+  var ix = this.params.rand(0, moves.length - 1);
   return {
       done: true,
-      move: ctx.childs[ix].move,
+      move: moves[ix],
       ai:   "random"
   };
 }
