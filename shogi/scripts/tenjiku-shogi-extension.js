48aa819956222665da62ec394f44dca478250f17
GlukKazan <glukkazan@gmail.com>
2017-07-13 18:02:12 +0300

    Tenjiku Shogi (experimental)

---
+++ shogi/scripts/tenjiku-shogi-extension.js
@@ -0,0 +1,59 @@
+(function() {
+
+var checkVersion = Dagaz.Model.checkVersion;
+
+Dagaz.Model.checkVersion = function(design, name, value) {
+  if (name != "tenjiku-shogi-extension") {
+     checkVersion(design, name, value);
+  }
+}
+
+var CheckInvariants = Dagaz.Model.CheckInvariants;
+
+Dagaz.Model.CheckInvariants = function(board) {
+  var design = Dagaz.Model.design;
+  var daemon = design.getPieceType("Fire-Demon");
+  var dirs   = design.allDirections();
+  for (var i = 0; i < board.moves.length; i++) {
+       var move = board.moves[i];
+       var action = null;
+       for (var j = 0; j < move.actions.length; j++) {
+            var a = move.actions[j];
+            if ((a[0] !== null) && (a[1] !== null)) {
+                 action = a;
+            }
+       }
+       if (action !== null) {
+            var isFired = false;
+            var pos  = action[1][0];
+            for (var k = 0; k < dirs.length; k++) {
+                 var p = design.navigate(board.player, pos, dirs[k]);
+                 if (p !== null) {
+                     var piece = board.getPiece(p);
+                     if ((piece !== null) && (piece.type == daemon) && (piece.player != board.player)) {
+                         isFired = true;
+                         break;
+                     }
+                 }
+            }
+            if (isFired) {
+                move.capturePiece(pos, action[3]);
+            } else {
+                if ((action[2] !== null) && (action[2][0].type == daemon)) {
+                    for (var k = 0; k < dirs.length; k++) {
+                         var p = design.navigate(board.player, pos, dirs[k]);
+                         if (p !== null) {
+                             var piece = board.getPiece(p);
+                             if ((piece !== null) && (piece.player != board.player)) {
+                                 move.capturePiece(p, action[3]);
+                             }
+                         }
+                    }
+                }
+            }
+       }
+  }
+  CheckInvariants(board);
+}
+
+})();
