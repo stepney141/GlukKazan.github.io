78737f24de2b0939432ba2336958bd0b0e6aaea3
GlukKazan <glukkazan@gmail.com>
2017-11-07 18:09:25 +0300

    Spock (completed)

---
+++ elimination/scripts/spock-extension.js
@@ -0,0 +1,37 @@
+(function() {
+
+var checkVersion = Dagaz.Model.checkVersion;
+
+Dagaz.Model.checkVersion = function(design, name, value) {
+  if (name != "spock-extension") {
+      checkVersion(design, name, value);
+  }
+}
+
+var CheckInvariants = Dagaz.Model.CheckInvariants;
+
+Dagaz.Model.CheckInvariants = function(board) {
+  var design = Dagaz.Model.design;
+  var spock  = design.getPieceType("Spock");
+  _.each(board.moves, function(m) {
+      if ((m.actions.length == 1) && (m.actions[0][0] !== null) && (m.actions[0][1] !== null)) {
+           var pos = m.actions[0][0][0];
+           var piece = board.getPiece(pos);
+           if ((piece !== null) && (piece.type == spock)) {
+                var target = m.actions[0][1][0];
+                var piece = board.getPiece(target);
+                if (piece !== null) {
+                    var type = piece.type + 1;
+                    if (type >= 5) {
+                        type = 0;
+                    }
+                    piece = Dagaz.Model.createPiece(type, board.player);
+                    m.dropPiece(pos, piece);
+                }
+           }
+      }
+  });
+  CheckInvariants(board);
+}
+
+})();
