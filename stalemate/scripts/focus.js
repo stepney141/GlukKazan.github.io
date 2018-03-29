a507f63b38fb6a1bc0655e10a5c54b72fcee6c50
GlukKazan <glukkazan@gmail.com>
2018-03-29 14:57:06 +0200

    show-hints=false

--- stalemate/scripts/focus.js
+++ stalemate/scripts/focus.js
@@ -24,6 +24,7 @@ Dagaz.Model.BuildDesign = function(design) {
     design.checkVersion("animate-captures", "false");
     design.checkVersion("smart-moves", "false");
     design.checkVersion("show-blink", "false");
+    design.checkVersion("show-hints", "false");
     design.checkVersion("shared-pieces", "true");
     design.checkVersion("focus-extension", "true");
 
--- stalemate/scripts/hunt.js
+++ stalemate/scripts/hunt.js
@@ -21,6 +21,9 @@ ZRF = {
 
 Dagaz.Model.BuildDesign = function(design) {
     design.checkVersion("z2j", "2");
+    design.checkVersion("smart-moves", "false");
+    design.checkVersion("show-hints", "false");
+    design.checkVersion("show-blink", "false");
 
     design.addDirection("nne");
     design.addDirection("e");
