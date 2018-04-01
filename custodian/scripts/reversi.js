5bd577bb24d2d39d3cc7d687bcc67b213d0fbba6
GlukKazan <glukkazan@gmail.com>
2018-04-01 15:31:20 +0200

    show-drops=all

--- custodian/scripts/reversi.js
+++ custodian/scripts/reversi.js
@@ -26,6 +26,7 @@ Dagaz.Model.BuildDesign = function(design) {
     design.checkVersion("smart-moves", "false");
     design.checkVersion("show-blink", "false");
     design.checkVersion("show-hints", "false");
+    design.checkVersion("show-drops", "all");
     design.checkVersion("pass-turn", "forced");
     design.checkVersion("reversi-extension", "true");
 
