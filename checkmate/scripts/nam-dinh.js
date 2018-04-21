ee0d3fbaa581bdbfeae616450447ddf62cd5e568
GlukKazan <glukkazan@gmail.com>
2018-04-21 10:00:03 +0200

    smart-moves=false

--- checkmate/scripts/nam-dinh.js
+++ checkmate/scripts/nam-dinh.js
@@ -22,7 +22,7 @@ ZRF = {
 Dagaz.Model.BuildDesign = function(design) {
     design.checkVersion("z2j", "2");
     design.checkVersion("animate-captures", "false");
-    design.checkVersion("smart-moves", "true");
+    design.checkVersion("smart-moves", "false");
     design.checkVersion("show-hints", "false");
     design.checkVersion("show-blink", "false");
     design.checkVersion("nam-dinh-invariant", "true");
