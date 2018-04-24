b4daac103971d9589d185ff6ee50caa4bfebd905
GlukKazan <glukkazan@gmail.com>
2018-04-24 16:53:45 +0200

    smart-moves=false

--- custodian/scripts/seega.js
+++ custodian/scripts/seega.js
@@ -23,7 +23,7 @@ Dagaz.Model.BuildDesign = function(design) {
     design.checkVersion("z2j", "2");
     design.checkVersion("animate-captures", "false");
     design.checkVersion("animate-drops", "false");
-    design.checkVersion("smart-moves", "true");
+    design.checkVersion("smart-moves", "false");
     design.checkVersion("show-blink", "false");
     design.checkVersion("show-hints", "false");
     design.checkVersion("pass-turn", "forced");
