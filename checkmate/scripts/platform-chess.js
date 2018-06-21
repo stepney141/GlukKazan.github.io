f9cc073e307da66044d3e12371b6e14ed72944e7
GlukKazan <glukkazan@gmail.com>
2018-06-21 17:40:24 +0200

    drag-n-drop option

--- checkmate/scripts/platform-chess.js
+++ checkmate/scripts/platform-chess.js
@@ -25,6 +25,7 @@ Dagaz.Model.BuildDesign = function(design) {
     design.checkVersion("smart-moves", "false");
     design.checkVersion("show-hints", "false");
     design.checkVersion("show-blink", "false");
+    design.checkVersion("drag-n-drop", "false");
 
     design.addDirection("w");
     design.addDirection("e");
