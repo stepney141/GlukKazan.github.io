a99ae41fda82116bf60fe6805f5af76ce8b6de44
GlukKazan <glukkazan@gmail.com>
2018-09-22 16:50:41 +0200

    Mancala (sounds)

--- mancala/scripts/mancala-view.js
+++ mancala/scripts/mancala-view.js
@@ -13,6 +13,10 @@ Dagaz.Model.checkVersion = function(design, name, value) {
   }
 }
 
+if (!_.isUndefined(Dagaz.Controller.addSound)) {
+    Dagaz.Controller.addSound(0, "../sounds/seed.ogg");
+}
+
 Dagaz.Model.getPieceType = function(piece) {
   var value = piece.getValue(0);
   if (value !== null) {
