07f6ea48c82c5f83abd072d6f832fce1221ec9fd
GlukKazan <glukkazan@gmail.com>
2019-02-13 09:45:44 +0300

    Game of Generals (fix)

--- races/scripts/gog.js
+++ races/scripts/gog.js
@@ -27,6 +27,7 @@ Dagaz.Model.BuildDesign = function(design) {
     design.checkVersion("animate-captures", "false");
     design.checkVersion("smart-moves", "false");
     design.checkVersion("show-blink", "false");
+    design.checkVersion("show-captures", "false");
     design.checkVersion("advisor-wait", "5");
 
     design.addDirection("s");
