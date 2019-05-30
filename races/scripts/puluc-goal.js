648bd2001c35c3f62221bc245eb9b2bc4c379f60
GlukKazan <glukkazan@gmail.com>
2019-05-30 14:54:59 +0300

    Puluc (ai)

--- races/scripts/puluc-goal.js
+++ races/scripts/puluc-goal.js
@@ -1,6 +1,7 @@
 (function() {
 
-var penalty = [40, 60, 40, 10, 10];
+var bonus = [9, 17, 19, 16, 14, 6, 3, 1];
+var penalty = [25, 38, 25, 6,  6];
 
 var checkVersion = Dagaz.Model.checkVersion;
 
@@ -15,45 +16,83 @@ if (!_.isUndefined(Dagaz.Controller.addSound)) {
     Dagaz.Controller.addSound(10, "../sounds/dice.wav", true);
 }
 
-var isEnemy = function(design, board, player, pos) {
-  var r = false;
-  var p = design.navigate(1, pos, 0);
-  while (p !== null) {
-      var piece = board.getPiece(p);
-      if (piece !== null) {
-          r = (piece.player != player);
-      }
-      p = design.navigate(1, p, 0);
+var getState = function(design, board, player, pos) {
+  var f = 0; var e = 0; var p = 0; var c = false;
+  for (; pos !== null; pos = design.navigate(player, pos, 0)) {
+       var piece = board.getPiece(pos);
+       if (piece !== null) {
+           if (piece.player == player) {
+               f++;
+           } else {
+               e++;
+           }
+           if ((board.lastt == pos) && (p != 0) && (p != player)) c = true;
+           p = piece.player;
+       }
+  }
+  return {
+       player:  p,
+       friends: f,
+       enemies: e,
+       capture: c
+  };
+}
+
+var getBonus = function(state, ix, player) {
+  var r = 0;
+  for (var i = 0; i < bonus.length; i++) {
+       if (ix + i >= state.length - 1) break;
+       if ((state[ix + i].player > 0) && (state[ix + i].player != player)) {
+           r += bonus[i];
+       }
+  }
+  return r;
+}
+
+var getPenalty = function(state, ix, player) {
+  var r = 0;
+  for (var i = 0; i < penalty.length; i++) {
+       if (ix + i >= state.length) break;
+       if ((state[ix + i].player > 0) && (state[ix + i].player != player)) {
+           r += penalty[i];
+       }
   }
   return r;
 }
 
 Dagaz.AI.eval = function(design, board, player) {
-  var r = 500;
-  _.each(design.allPositions(), function(pos) {
-      var piece = board.getPiece(pos);
-      if (piece === null) return;
-      if (piece.type != 2) return;
-      var v = 100;
-      if (!design.inZone(1, piece.player, pos)) {
-          var p = design.navigate(1, pos, 0);
-          if ((p !== null) && (board.getPiece(p) !== null)) {
-              v = 10;
-          } else {
-              p = design.navigate(1, pos, 2);
-              for (var ix = 0; ix < penalty.length; ix++) {
-                  if (p === null) break;
-                  if (design.inZone(1, piece.player, p)) break;
-                  if (isEnemy(design, board, piece.player, p)) {
-                      v -= penalty[ix];
-                  }
-                  p = design.navigate(1, p, 2);
-              }
-          }
-      }
-      if (piece.player != player) v = -v;
-      r += v;
-  });
+  var r = 0;
+  var pos = Dagaz.Model.stringToPos("a1");
+  if (player != 1) {
+      pos = Dagaz.Model.stringToPos("a11");
+  }
+  var state = [];
+  while (pos !== null) {
+      state.push(getState(design, board, player, pos));
+      pos = design.navigate(player, pos, 2);
+      if ((pos !== null) && design.inZone(1, player, pos)) break;
+  }
+  var cnt = 0;
+  for (var i = 0; i < state.length; i++) {
+       if (state[i].player > 0) {
+           if (state[i].player == player) {
+               r += 100 - (state[i].friends * 10);
+               r += state[i].enemies * 100;
+               r += getBonus(state, i, player);
+               if (i > 0) {
+                   r -= getPenalty(state, i, player);
+               }
+               cnt += state[i].enemies;
+           } else {
+               cnt += state[i].friends;
+           }
+           if (state[i].capture) {
+               r += 5000;
+           }
+       }
+  }
+  r += (5 - cnt) * 1000;
+  console.log("Move: " + board.move.toString() + ", eval = " + r);
   return r;
 }
 
