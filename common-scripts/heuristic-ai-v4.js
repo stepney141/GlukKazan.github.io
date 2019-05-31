815540294914c26d4f66ff355ca8e024c8e0f28e
GlukKazan <glukkazan@gmail.com>
2019-05-31 12:20:01 +0300

    Hypergammon (no impasse)

--- common-scripts/heuristic-ai-v4.js
+++ common-scripts/heuristic-ai-v4.js
@@ -68,6 +68,7 @@ Ai.prototype.getMove = function(ctx) {
       return { done: true, ai: "nothing" };
   }
   if (_.isUndefined(ctx.moves) || (ctx.moves.length == 0)) {
+      Dagaz.Model.noRecursive = false;
       var nodes = _.map(ctx.board.moves, function(move) {
           return {
              o: ctx.board.player,
@@ -90,6 +91,7 @@ Ai.prototype.getMove = function(ctx) {
           ctx.moves.push(best.m);
           best = best.p;
       }
+      Dagaz.Model.noRecursive = true;
   }
   if (ctx.moves.length > 0) {
       return {
