d7a13c5d1f65851067a94dcf44e9453af6bf37b9
GlukKazan <glukkazan@gmail.com>
2019-07-11 15:53:39 +0300

    Oblong Shatranj (fixed)

--- checkmate/scripts/oblong-pass.js
+++ checkmate/scripts/oblong-pass.js
@@ -13,7 +13,9 @@ var CheckInvariants = Dagaz.Model.CheckInvariants;
 Dagaz.Model.CheckInvariants = function(board) {
   var design = Dagaz.Model.design;
   var pos = design.positions.length - 1;
+  var cnt = 0;
   _.each(board.moves, function(move) {
+      if (!_.isUndefined(move.failed)) return;
       if (move.mode == 0) {
           var dice = design.price[move.actions[0][2][0].type];
           if (dice > 1) {
@@ -26,8 +28,16 @@ Dagaz.Model.CheckInvariants = function(board) {
           if (piece !== null) {
               move.capturePiece(pos);
           }
+          cnt++;
       }
   });
+  if (cnt == 0) {
+      _.each(board.moves, function(move) {
+          if (move.mode == 7) {
+              move.failed = true;
+          }
+      });
+  }
   CheckInvariants(board);
 }
 
