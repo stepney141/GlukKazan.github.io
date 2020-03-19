15a1e75f1a0cdb30bb1dfcd42f236c9a808fe26a
GlukKazan <glukkazan@gmail.com>
2020-03-19 15:20:40 +0300

    Renju (minimal invariant)

--- renju/scripts/renju-invariant.js
+++ renju/scripts/renju-invariant.js
@@ -20,75 +20,6 @@ var addKo = function(board, move) {
   }
 }
 
-var isFork = function(a) {
-  if (a.length < 2) return false;
-  if (a.length > 2) return true;
-  if ((a[0] == 4) && (a[1] == 4)) return true;
-  if ((a[0] == 3) && (a[1] == 3)) return true;
-  return false;
-}
-
-var getLine = function(design, board, player, pos, dir, ix) {
-  var r = 0;
-  var p = design.navigate(player, pos, dir);
-  if (p === null) return 0;
-  var piece = board.getPiece(p);
-  while (piece !== null) {
-      if (piece.player != board.player) break;
-      var v = +piece.getValue(ix);
-      if (r < v) r = v;
-      p = design.navigate(player, p, dir);
-      if (p === null) break;
-      piece = board.getPiece(p);
-  }
-  return r;
-}
-
-var createPiece = function(design, board, player, pos) {
-  var dirs = [];
-  dirs.push(design.getDirection("n")); dirs.push(design.getDirection("ne"));
-  dirs.push(design.getDirection("e")); dirs.push(design.getDirection("se"));
-  dirs.push(design.getDirection("s")); dirs.push(design.getDirection("sw"));
-  dirs.push(design.getDirection("w")); dirs.push(design.getDirection("nw"));
-  var r = Dagaz.Model.createPiece(0, player);
-  for (var ix = 0; ix < 4; ix++) {
-      var a = getLine(design, board, player, pos, dirs[ix], ix);
-      var b = getLine(design, board, player, pos, dirs[ix + 4], ix);
-      r = r.setValue(ix, a + b + 1);
-  }
-  return r;
-}
-
-var getRank = function(design, board, pos, dir, ix, forced) {
-  var dirs  = [];
-  dirs.push(design.getDirection("n")); dirs.push(design.getDirection("ne"));
-  dirs.push(design.getDirection("e")); dirs.push(design.getDirection("se"));
-  dirs.push(design.getDirection("s")); dirs.push(design.getDirection("sw"));
-  dirs.push(design.getDirection("w")); dirs.push(design.getDirection("nw"));
-  var p = design.navigate(board.player, pos, dir);
-  if (p === null) return 0;
-  var piece = board.getPiece(p);
-  while (piece !== null) {
-      if (piece.player != board.player) return 0;
-      p = design.navigate(board.player, p, dir);
-      if (p === null) return 0;
-      piece = board.getPiece(p);
-  }
-  piece = createPiece(design, board, board.player, p);
-  if (piece.getValue(ix) == 5) return 4;
-  if (forced) {
-  p = design.navigate(board.player, p, dir);
-  if ((p === null) || (board.getPiece(p) !== null)) return 0;
-  if (piece.getValue(ix) == 4) return 3;
-      board.setPiece(p, piece);
-      var a = getRank(design, board, p, dirs[ix], ix, false);
-      var b = getRank(design, board, p, dirs[ix + 4], ix, false);
-      board.setPiece(p, null);
-      if ((a == 4) && (b == 4)) return 3;
-  }
-  return 0;
-}
-
 var CheckInvariants = Dagaz.Model.CheckInvariants;
 
 Dagaz.Model.CheckInvariants = function(board) {
@@ -104,7 +35,6 @@ Dagaz.Model.CheckInvariants = function(board) {
           var pos    = move.actions[0][1][0];
           var piece  = move.actions[0][2][0];
           var result = [];
-          board.setPiece(pos, piece);
           for (var ix = 0; ix < 4; ix++) {
                var v = +piece.getValue(ix);
                if (v > 5) {
@@ -112,24 +42,6 @@ Dagaz.Model.CheckInvariants = function(board) {
                    move.failed = true;
                    return;
                }
-               var a = getRank(design, board, pos, dirs[ix], ix, true);
-               var b = getRank(design, board, pos, dirs[ix + 4], ix, true);
-               if ((a == 4) && (b == 4)) {
-                    if (v < 4) result.push(4);
-                    result.push(4);
-               }
-               if (v < 3) {
-                   if ((a == 3) && (b != 4)) result.push(3);
-                   if ((a != 4) && (b == 3)) result.push(3);
-               } else {
-                   if ((a == 3) || (b == 3)) result.push(3);
-               }
-          }
-          board.setPiece(pos, null);
-          if (isFork(result)) {
-              addKo(board, move);
-              move.failed = true;
-              return;
           }
       });
   }
