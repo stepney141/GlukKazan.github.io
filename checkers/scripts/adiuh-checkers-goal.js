fb084e880915b0410b21f3190a3c9e2a86a79ce9
GlukKazan <glukkazan@gmail.com>
2021-03-03 16:52:05 +0300

    Adiuh Checkers (completed)

--- checkers/scripts/adiuh-checkers-goal.js
+++ checkers/scripts/adiuh-checkers-goal.js
@@ -8,39 +8,32 @@ Dagaz.Model.checkVersion = function(design, name, value) {
   }
 }
 
-var showDigits = function(view, ctx, x, y, v) {
-  for (var i = 0; i < v.length; i++) {
-       var r = view.piece[v[i]];
-       if (r) {
-           ctx.drawImage(r.h, x, y, r.dx, r.dy);
-       }
-       x += 22;
-  }
-}
-
-Dagaz.View.showBoard = function(board, ctx) {
-  var design = Dagaz.Model.design;
-  var w = 0; var b = 0;
-  for (var pos = 0; pos < 64; pos++) {
-       var piece = board.getPiece(pos);
-       if (piece !== null) {
-           var c = 1;
-           var s = piece.getValue(0);
-           if (s !== null) {
-               c += s.length;
-           }
-           if (pos < 32) {
-               w += c;
-           } else {
-               b += c;
-           }
-       }
-  }
-  w = w.toString(10);
-  b = b.toString(10);
-  var view = Dagaz.Controller.app.view;
-  showDigits(view, ctx, 0, 5, b);
-  showDigits(view, ctx, 548 - (w.length * 22), 5, w);
+Dagaz.AI.eval = function(design, params, board, player) {
+  var r = 0;
+  _.each(design.allPositions(), function(pos) {
+      var piece = board.getPiece(pos);
+      if (piece === null) return;
+      var v = piece.getValue(0);
+      if (v === null) {
+          v = 1;
+      } else {
+          v++;
+      }
+      if (pos >= 32) {
+          v = -v;
+      }
+      if (player == 1) {
+          r += v;
+      } else {
+          r -= v;
+      }
+      if (piece.player == player) {
+          r++;
+      } else {
+          r--;
+      }
+  });
+  return r;
 }
 
 var checkGoals = Dagaz.Model.checkGoals;
@@ -64,20 +57,25 @@ Dagaz.Model.checkGoals = function(design, board, player) {
                }
            }
       }
-      if (w == b) return 0;
+      var r = null;
+      if (w == b) r = 0;
       if (w > b) {
           if (player == 1) {
-              return 1;
+              r = 1;
           } else {
-              return -1;
+              r = -1;
           }
       } else {
           if (player == 1) {
-              return -1;
+              r = -1;
           } else {
-              return 1;
+              r = 1;
           }
       }
+      if (r !== null) return {
+          message: ' (' + w + ':' + b + ')',
+          result: r
+      };
   }
   return checkGoals(design, board, player);
 }
