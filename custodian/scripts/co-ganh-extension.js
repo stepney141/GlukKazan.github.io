3b81a2fea896a55304d625049d923a5299b49f21
GlukKazan <glukkazan@gmail.com>
2021-04-26 10:32:09 +0300

    Co ganh (fixed)

--- custodian/scripts/co-ganh-extension.js
+++ custodian/scripts/co-ganh-extension.js
@@ -22,14 +22,33 @@ Dagaz.Model.CheckInvariants = function(board) {
       var b = board.apply(move);
       var pos = move.actions[0][1][0];
       var captured = [];
+      var from = move.actions[0][0][0];
       _.each(design.allDirections(), function(dir) {
           var p = design.navigate(board.player, pos, dir);
-          if (p === null) return;
-          var piece = b.getPiece(p);
+          if ((p === null) || (p == from)) return;
+          var q = design.navigate(0, pos, dir);
+          if ((q === null) || (q == from)) return;
+          var x = b.getPiece(p); var y = b.getPiece(q);
+          if ((x === null) || (x.player == board.player)) return;
+          if ((y === null) || (y.player == board.player)) return;
+          if (_.indexOf(captured, p) < 0) captured.push(p);
+          if (_.indexOf(captured, q) < 0) captured.push(q);
+      });
+      var piece = Dagaz.Model.createPiece(0, board.player);
+      _.each(captured, function(pos) {
+          move.movePiece(pos, pos, piece);
+      });
+  });
+  _.each(board.moves, function(move) {
+      if (move.actions.length < 1) return;
+      var b = board.apply(move);
+      var captured = [];
+      _.each(design.allPositions(), function(pos) {
+          if (_.indexOf(captured, pos) >= 0) return;
+          var piece = b.getPiece(pos);
           if (piece === null) return;
           if (piece.player == board.player) return;
-          if (_.indexOf(captured, p) >= 0) return;
-          var group = [p]; var dame = 0;
+          var group = [pos]; var dame = 0;
           for (var i = 0; i < group.length; i++) {
                _.each(design.allDirections(), function(d) {
                    var q = design.navigate(board.player, group[i], d);
@@ -47,18 +66,6 @@ Dagaz.Model.CheckInvariants = function(board) {
           if (dame == 0) {
               captured = _.union(captured, group);
           }
-      });
-      var from = move.actions[0][0][0];
-      _.each(design.allDirections(), function(dir) {
-          var p = design.navigate(board.player, pos, dir);
-          if ((p === null) || (p == from)) return;
-          var q = design.navigate(0, pos, dir);
-          if ((q === null) || (q == from)) return;
-          var x = b.getPiece(p); var y = b.getPiece(q);
-          if ((x === null) || (x.player == board.player)) return;
-          if ((y === null) || (y.player == board.player)) return;
-          if (_.indexOf(captured, p) < 0) captured.push(p);
-          if (_.indexOf(captured, q) < 0) captured.push(q);
       });
       var piece = Dagaz.Model.createPiece(0, board.player);
       _.each(captured, function(pos) {
