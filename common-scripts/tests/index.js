e673bc67ddf9ba549ec8af89656a68c57077f63d
stepney141 <stepney141andi.c.i@gmail.com>
2021-05-06 03:36:45 +0900

    refactor: fixed an E2E test on Doubutsu Shogi

--- common-scripts/tests/index.js
+++ common-scripts/tests/index.js
@@ -1,49 +1,60 @@
-const puppeteer = require('puppeteer');
+import puppeteer from 'puppeteer';
 
 const url = process.argv[2];
 
 (async () => {
-  const browser = await puppeteer.launch({
-    defaultViewport: {
-      width: 600,
-      height: 700,
-    },
-    headless: false,
-  });
   try {
+    const browser = await puppeteer.launch({
+      defaultViewport: {
+        width: 600,
+        height: 700,
+      },
+      headless: true,
+      // devtools: true
+    });
     const page = await browser.newPage();
     const mouse_click = async (x, y, time) => {
-      try {
-        await Promise.all([
-          page.mouse.move(x, y),
-          page.waitForTimeout(time),
-          page.mouse.click(x, y)
-        ]);
-        return true;
-      } catch (e) {
-        const message = 'mouse_click_error:';
-        console.logl(message + e);
-        return false;
-      }
+      await Promise.all([
+        page.mouse.move(x, y),
+        page.waitForTimeout(time),
+        page.mouse.click(x, y)
+      ]);
     };
       
     await page.goto(url, { waitUntil: "networkidle2" });
       
-    await mouse_click(260, 366, 10000);
-    await mouse_click(167, 273, 10000);
-      
-    await mouse_click(260, 280, 10000);
-    await mouse_click(260, 185, 10000);
+    await mouse_click(260, 366, 1000);
+    await page.waitForTimeout(2000);
+    await mouse_click(167, 273, 1000);
 
-    await mouse_click(163, 367, 10000);
-    await mouse_click(260, 280, 10000);
+    console.log("3三らいおん 1二らいおん");
       
-    await page.waitForTimeout(10000);
+    await page.waitForTimeout(2000);
+
+    await mouse_click(260, 280, 1000);
+    await page.waitForTimeout(2000);
+    await mouse_click(255, 180, 1000);
+
+    console.log("2二ひよこ 2一きりん");
+
+    await page.waitForTimeout(2000);
+
+    await mouse_click(163, 367, 1000);
+    await page.waitForTimeout(2000);
+    await mouse_click(260, 280, 1000);
+
+    console.log("2三ぞう");
+
+    page.on('dialog', async (dialog) => {
+      console.log(await dialog.message());
+      await dialog.dismiss();
+    });
+      
+    await page.waitForTimeout(1000);
+    await browser.close();
       
   } catch (e) {
     console.log(e);
-  } finally {
-    await browser.close();
   }
     
-});
+})();
