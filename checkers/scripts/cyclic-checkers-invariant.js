d4e3dc80440fa5cb596d7d6b599003da3d4dd6a7
GlukKazan <glukkazan@gmail.com>
2021-06-16 11:45:30 +0300

    Cyclic Checkers (invariant fixed)

--- checkers/scripts/cyclic-checkers-invariant.js
+++ checkers/scripts/cyclic-checkers-invariant.js
@@ -8,35 +8,49 @@ Dagaz.Model.checkVersion = function(design, name, value) {
   }
 }
 
-var getLen = function(board, move) {
-  var r = 0;
-  var f = false;
-  _.each(move.actions, function(a) {
-      if (f) return;
-      if (a[0] === null) return;
-      if (a[1] !== null) return;
-      var piece = board.getPiece(a[0][0]);
-      if (piece === null) return;
-      if (piece.player == board.player) {
-          f = true;
-      } else {
-          r++;
-      }
-  });
-  return r;
+var isEq = function(a, b) {
+  if ((a[0] !== null) && (b[0] === null)) return false;
+  if ((a[0] === null) && (b[0] !== null)) return false;
+  if ((a[0] !== null) && (a[0][0] != b[0][0])) return false;
+  if ((a[1] !== null) && (b[1] === null)) return false;
+  if ((a[1] === null) && (b[1] !== null)) return false;
+  if ((a[1] !== null) && (a[1][0] != b[1][0])) return false;
+  return true;
+}
+
+var isPrefix = function(board, prefix, move, pn) {
+  if (prefix.actions.length < move.actions.length) return false;
+  var i = 0;
+  for (; i < prefix.actions.length; i++) {
+      if (prefix.actions[i][3] >= pn) break;
+      if (!isEq(prefix.actions[i], move.actions[i])) return false;
+  }
+  if (prefix.actions[i][0] === null) return false;
+  if (prefix.actions[i][1] !== null) return false;
+  var piece = board.getPiece(prefix.actions[i][0][0]);
+  if (piece === null) return false;
+  return piece.player != board.player;
 }
 
 var CheckInvariants = Dagaz.Model.CheckInvariants;
 
 Dagaz.Model.CheckInvariants = function(board) {
-  var len = 0;
+  var design = Dagaz.Model.design;
   _.each(board.moves, function(move) {
-      var l = getLen(board, move);
-      if (len < l) len = l;
-  });
-  _.each(board.moves, function(move) {
-      var l = getLen(board, move);
-      if (l < len) move.failed = true;
+      var pn = 0;
+      _.each(move.actions, function(a) {
+          if (pn > 0) return;
+          if (a[0] === null) return;
+          if (a[1] !== null) return;
+          var piece = board.getPiece(a[0][0]);
+          if (piece === null) return;
+          if (piece.player !== board.player) return;
+          pn = a[3];
+      });
+      if (pn == 0) return;
+      _.each(board.moves, function(prefix) {
+          if (isPrefix(board, prefix, move, pn)) move.failed = true;
+      });
   });
   CheckInvariants(board);
 }
